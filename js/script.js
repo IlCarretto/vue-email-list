651eef0b553022673c921b94b1fd77156dffb8af
IlCarretto <carlorebuffat412@gmail.com>
2022-10-28 17:28:25 +0200

    done with bonsu

---
+++ js/script.js
@@ -0,0 +1,31 @@
+// Attraverso l'apposita API di Boolean
+// https://flynn.boolean.careers/exercises/api/random/mail
+// generare 10 indirizzi email e stamparli in pagina all'interno di una lista.
+
+// Bonus
+// Far comparire gli indirizzi email solamente quando sono stati tutti generati.
+
+const {createApp} = Vue;
+
+createApp({
+    data() {
+        return {
+            emailsList: []
+        }
+    },
+    methods: {
+        printEmails() {
+            for (let i = 0; i < 10; i++) {
+                axios
+                .get("https://flynn.boolean.careers/exercises/api/random/mail")
+                .then((resp) => {
+                    this.emailsList.push(resp.data.response);
+                });
+            }
+        }  
+    },
+    created() {
+        this.printEmails()
+        console.log(this.emailsList);
+    }
+}).mount("#app");
